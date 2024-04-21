af76312006e5b3e243d503a2ef9f78155ac431d5
Golen <golen87@hotmail.com>
2024-04-21 13:45:57 +0200

    Remove Rex plugin

--- src/game.ts
+++ src/game.ts
@@ -2,7 +2,6 @@ import Phaser from "phaser";
 import { PreloadScene } from "@/scenes/PreloadScene";
 import { TitleScene } from "@/scenes/TitleScene";
 import { GameScene } from "@/scenes/GameScene";
-import OutlinePipelinePlugin from "phaser3-rex-plugins/plugins/outlinepipeline-plugin.js";
 
 const config: Phaser.Types.Core.GameConfig = {
 	type: Phaser.WEBGL,
@@ -14,16 +13,6 @@ const config: Phaser.Types.Core.GameConfig = {
 		mode: Phaser.Scale.FIT,
 	},
 	scene: [PreloadScene, TitleScene, GameScene],
-
-	plugins: {
-		global: [
-			{
-				key: "rexOutlinePipeline",
-				plugin: OutlinePipelinePlugin,
-				start: true,
-			},
-		],
-	},
 };
 
 const game = new Phaser.Game(config);
